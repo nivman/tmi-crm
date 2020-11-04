5d781ff39ac448e9205d52b6c2c65ead36f253d7
nivman <nivman1980@gmail.com>
2020-11-04 14:33:05 +0200

    minor translate

--- resources/js/components/expenses/ExpenseListComponent.vue
+++ resources/js/components/expenses/ExpenseListComponent.vue
@@ -6,9 +6,9 @@
                     <i class="fas fa-cog" />
                 </button>
                 <router-link to="/expenses/add" class="button is-link is-small is-pulled-right">
-                    <i class="fas fa-plus m-r-sm" /> Add Expense
+                    <i class="fas fa-plus m-r-sm" /> הוספת הוצאה
                 </router-link>
-                Expenses
+                הוצאות
                 <i v-if="loading" class="fas fa-spinner fa-pulse"></i>
             </div>
             <div class="panel-block table-body-br">
